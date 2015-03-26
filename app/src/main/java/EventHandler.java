2ac84f9a348489aafc392b4aecdb8007734f138d
TenLetters <10letterz@gmail.com>
2015-03-26 18:08:31 -0400

    implemented StatusEffects and a Warrior Class

--- app/src/main/java/EventHandler.java
+++ app/src/main/java/EventHandler.java
@@ -1,7 +1,10 @@
+import android.media.Image;
+
 import java.util.List;
 
 import JavaFiles.*;
 import JavaFiles.Character;
+import JavaFiles.Characters.MoveResult;
 
 /**
  * Created by AlexC on 3/12/2015.
@@ -10,13 +13,15 @@ public class EventHandler {
 
     private List<Character> players;
     private List<Character> enemies;
+    private Image backgroundImage;
 
 
     // Constructor for an object of type EventHandler
-    public EventHandler(List<Character> players, List<Character> enemies)
+    public EventHandler(List<Character> players, List<Character> enemies, Image backgroundImage)
     {
         this.players = players;
         this.enemies = enemies;
+        this.backgroundImage = backgroundImage;
     }
 
     // Called by the Event Activity
@@ -30,21 +35,26 @@ public class EventHandler {
 
         // apply the move
         // returns a list with the damage in the first index and the names of any status effects applied afterwards
-        List<String> moveResult = target.hitByEffect(moveEffect);
+        MoveResult moveResult = target.hitByEffect(moveEffect);
+        // unpack the message from the MoveResult
+        List<String> moveResultMessage = moveResult.getResultMessage();
+        //un pack the resulting character from the MoveResult
+        // and save its reference to overwrite the current target
+        target = moveResult.getResultCharacter();
 
         // break down our result to a single, formatted string
         String result = user.getName() + " used " + moveName + " against " + target.getName();
         // check if any damage was done and we need to report it
-        if(Integer.parseInt(moveResult.get(0)) != 0) {
-            result += " for " + moveResult.get(0) + " damage ";
+        if(Integer.parseInt(moveResultMessage.get(0)) != 0) {
+            result += " for " + moveResultMessage.get(0) + " damage ";
         }
         // check if any status effects were applied and we need to report them
-        if(moveResult.size() > 1)
+        if(moveResultMessage.size() > 1)
         {
             result += " which inflicted: ";
             // add names of all status effects used from the rest of the list, skipping the first slot
             // which contains the damage
-            for(String effectName : moveResult.subList(1,moveResult.size() - 1))
+            for(String effectName : moveResultMessage.subList(1,moveResultMessage.size() - 1))
             {
                 result += effectName;
                 result += " ";
--- app/src/main/java/JavaFiles/Character.java
+++ app/src/main/java/JavaFiles/Character.java
@@ -3,6 +3,8 @@ package JavaFiles;
 import java.util.ArrayList;
 import java.util.List;
 
+import JavaFiles.Characters.MoveResult;
+
 /**
  * Created by AlexC on 3/10/2015.
  */
@@ -48,8 +50,8 @@ public abstract class Character {
         return null;
     }
 
-    // applies a status effect to this character
-    protected abstract List<Character> applyStatusEffect(List<Character> effects);
+    // checks which status effects this character already has and returns a list with all new ones to be applied
+    protected abstract List<Character> checkAppliedStatusEffects(List<Character> effects);
     // if its a status effect class, remove its name from the list of effects if it is there; possibly refresh time on effect?
     // then call the same method on the character object it stores
     // if its a default job class then return the list of status effects as it remains
@@ -57,9 +59,8 @@ public abstract class Character {
     // which hitByEffect was called on so return the list back there
 
 
-
     // applies the effect of a move on this character
-    public abstract List<String> hitByEffect(Effect effect);
+    public abstract MoveResult hitByEffect(Effect effect);
     // get the damage the move inflicts
     //int damage = effect.getBase_damage();
     // modify damage if this status effect does that!!!
@@ -67,7 +68,7 @@ public abstract class Character {
     //stat.modifyHealth(damage); *** DO THE RECURSION IN THIS CALL
 
     // get the status effects from the move that were applied
-    //List<Character> effects = applyStatusEffect(effect.getStatus_effects()
+    //List<Character> effects = applyStatusEffect(effect.getStatus_effects());
     // status effects add their name to a list and call the next method, continue until we hit the base class
     // then pass a list of currently afflicted statuses back up
 
@@ -81,4 +82,7 @@ public abstract class Character {
     //}
     // return the list with the names of first, the damage inflicted, and secondly the names of all effects applied
     //return result;
+
+    // applies the status effects stored in the list
+    public abstract Character applyStatusEffects(List<Character> effects);
 }
